ff3effd3a7f9db9c0144cef6cdd2d2d52ae9c2b9
Javier Julio <jjfutbol@gmail.com>
2020-09-04 16:33:41 -0400

    Add solver with tests (used with permission)
    This is a port of the Nonogram solver by fedimser and used with permission.
    https://fedimser.github.io/nonogram
    https://github.com/fedimser/fedimser.github.io/blob/master/js/Nonogram.js
    
    We need the solver to be used in conjunction with the random generator of puzzles so that way we can confirm if a puzzle is uniquely solveable. If not, then we generate a new one, and repeat.

---
+++ src/solver.js
@@ -0,0 +1,227 @@
+// A port of the Nonogram solver by fedimser
+// https://fedimser.github.io/nonogram
+// https://github.com/fedimser/fedimser.github.io/blob/master/js/Nonogram.js
+
+class Line {
+  constructor(groups) {
+    this.groups = groups;
+    this.gn = groups.length;
+
+    if (this.gn > 0) {
+      this.restLength = new Array(this.gn).fill(0)
+      this.restLength[this.gn - 1] = groups[this.gn - 1];
+      for (var i = this.gn - 2; i >= 0; i--) {
+        this.restLength[i] = groups[i] + 1 + this.restLength[i + 1];
+      }
+    }
+  }
+
+  setCells(cells) {
+    this.length = cells.length;
+    this.cells = cells;
+
+    this.sure = new Array(this.length).fill(0)
+    for (var i = 0; i < this.length; i++) {
+      if (this.cells[i] != 0)
+        this.sure[i] = 1;
+    }
+
+    this.cur = new Array(this.length).fill(0);
+    this.ansLine = new Array(this.length).fill(0);
+  }
+
+  checkFinal(pos) {
+    for (var i = pos; i < this.length; i++)
+      if (this.cells[i] == 1)
+        return;
+
+    for (var i = 0; i < this.length; i++) {
+      if (this.ansLine[i] == 0)
+        this.ansLine[i] = this.cur[i];
+      else if (this.ansLine[i] != this.cur[i]) {
+        this.ansLine[i] = 2;
+        this.cells[i] = 0; this.sure[i] = 1;
+      }
+    }
+    this.realFound++;
+  }
+
+  rec(g, pos) {
+    if (this.realFound > 0)
+      return;
+
+    if (pos + this.restLength[g] > this.length)
+      return;
+
+    var ok = true;
+    for (var i = pos; i < pos + this.groups[g]; i++) {
+      if (this.cells[i] == -1) {
+        ok = false;
+        break;
+      }
+      this.cur[i] = 1;
+    }
+
+    if (pos + this.groups[g] < this.length && this.cells[pos + this.groups[g]] == 1) {
+      ok = false;
+    }
+
+    if (ok) {
+      if (g == this.gn - 1)
+        this.checkFinal(pos + this.groups[g]);
+      else {
+        for (var i = pos + this.groups[g] + 1; i < this.length; ++i) {
+          this.rec(g + 1, i);
+          if (this.cells[i] == 1)
+            break;
+        }
+      }
+    }
+
+    for (var i = pos; i < pos + this.groups[g]; i++) {
+      this.cur[i] = 0;
+    }
+  }
+
+  isFeasible() {
+    if (this.gn == 0) {
+      for (var i = 0; i < this.length; ++i)
+        if (this.cells[i] == 1)
+          return false;
+      return true;
+    }
+
+    this.realFound = 0;
+    for (var i = 0; i < this.length; ++i) {
+      //console.log("TRYING FROM " + i + "...");
+      this.rec(0, i);
+      if (this.cells[i] == 1)
+        break;
+    }
+    return (this.realFound != 0);
+  }
+
+  isModificationFeasible(pos, val) {
+    if (this.ansLine[pos] == 2 || this.ansLine[pos] == val)
+      return true;
+
+    var tmp = this.cells[pos];
+    this.cells[pos] = val;
+    var ans = this.isFeasible();
+    this.cells[pos] = tmp;
+    return ans;
+  }
+
+  solve() {
+    if (!this.isFeasible())
+      return false;
+
+    for (var i = 0; i < this.length; ++i) {
+      // console.log("Checking "+i+" ...");
+      if (this.sure[i] == 1)
+        continue;
+
+      if (!this.isModificationFeasible(i, 1))
+        this.cells[i] = -1;
+      else if (!this.isModificationFeasible(i, -1))
+        this.cells[i] = 1;
+      else
+        this.cells[i] = 0;
+
+      this.sure[i] = 1;
+    }
+
+    return true;
+  }
+}
+
+class Nonogram {
+  constructor(rowHints, columnHints) {
+    this.width = columnHints.length;
+    this.height = rowHints.length;
+
+    // this.matrix = new Array(this.height).fill().map(() => new Array(this.width).fill(0))
+    // this.matrix = Array.from({length: this.height}, () => new Array(this.width).fill(0))
+    this.matrix = Array.from({length: this.height}, () => new Int8Array(this.width))
+
+    // this.rows = [];
+    // this.columns = [];
+
+    this.rows = rowHints.map((hint) => new Line(hint))
+    this.columns = columnHints.map((hint) => new Line(hint))
+
+    // this.rows = new Array(this.height).fill().map((_, i) => new Line(rowHints[i]));
+
+    // console.log(temprows, this.rows, JSON.stringify(temprows))
+    // console.log(JSON.stringify(temprows) === JSON.stringify(this.rows))
+
+    // this.rows = Array.from({length: this.height}, (v, i) => new Line(rowHints[i]));
+    // this.columns = new Array(this.width).fill().map((_, i) => new Line(columnHints[i]));
+
+    // for (var i = 0; i < this.height; i++) this.rows.push(new Line(rowHints[i]));
+    // for (var i = 0; i < this.width; i++) this.columns.push(new Line(columnHints[i]));
+  }
+
+  getColumn(j) {
+    // let result = new Array(this.height).fill().map((_, i) => this.matrix[i][j]);
+    return Array.from({length: this.height}, (_, i) => this.matrix[i][j]);
+    return result;
+    // var ans = [];
+    // for (var i = 0; i < this.height; i++)
+    //   ans.push(this.matrix[i][j]);
+    // return ans;
+  }
+
+  updateMatrix(x, y, value) {
+    if (this.matrix[x][y] == 0 && value != 0) {
+      this.matrix[x][y] = value;
+      this.changed = true;
+    }
+  }
+
+  isComplete() {
+    for (var i = 0; i < this.height; i++)
+      for (var j = 0; j < this.width; j++)
+        if (this.matrix[i][j] == 0) return false;
+    // for (const row of this.matrix)
+    //   for (const col of row)
+    //     if (col == 0) return false;
+    return true;
+  }
+
+  solve() {
+    do {
+      this.changed = false;
+      for (var i = 0; i < this.height; i++) {
+        this.rows[i].setCells(this.matrix[i]);
+        if (!this.rows[i].solve()) return false;
+        for (var j = 0; j < this.width; j++) this.updateMatrix(i, j, this.rows[i].cells[j]);
+      }
+
+      for (var i = 0; i < this.width; i++) {
+        this.columns[i].setCells(this.getColumn(i));
+        if (!this.columns[i].solve()) return false;
+        for (var j = 0; j < this.height; j++) this.updateMatrix(j, i, this.columns[i].cells[j]);
+      }
+    } while (this.changed);
+    return true;
+  }
+
+  solvable() {
+    if (!this.solve()) return false;  // Impossible
+    if (!this.isComplete()) return false  // Multiple Solutions
+    return true
+  }
+}
+
+const solvable = (rowHints, columnHints) => {
+  let nonogram = new Nonogram(rowHints, columnHints)
+  let result = nonogram.solvable()
+  // convert -1 to 0, otherwise matrix is only 1 or -1 values, we need 0 for empty cells
+  // console.log(nonogram.matrix.map(row => row.map(col => (col === -1) ? 0 : col)))
+  return result
+
+  // return new Nonogram(rowHints, columnHints).solvable()
+}
+
+export default solvable;
---
+++ src/solver.test.js
@@ -0,0 +1,207 @@
+import solvable from './solver';
+import seedrandom from 'seedrandom';
+
+let draw = (rowHints, columnHints, rows) => {
+  let result = '';
+  let maxLength = a => a.map(x => x.length).reduce((max, i) => i > max ? i : max, 0);
+
+  let joinedRowHints = rowHints.map(x => x.join(' '));
+  let maxRowHintLength = maxLength(joinedRowHints);
+
+  let colHints = columnHints.map(x => x.join(' '));
+  let maxColHintLength = maxLength(colHints);
+  colHints = colHints.map(x => x.padStart(maxColHintLength).split(''));
+
+  for (let i = 0; i < maxColHintLength; i++) {
+    let n = colHints.map(x => x.shift()).join('');
+    result += ''.padStart(maxRowHintLength);
+    result += ' ' + n;
+    result += '\n';
+  }
+  // result += '\n';
+
+  rows.forEach((content, i) => {
+    let art = content.map(x => {
+      if (x === -1) {
+        return 'x';
+      }
+      return ['░', '█'][x]
+    }).join('');
+    result += `${joinedRowHints[i].padStart(maxRowHintLength)} ${art}`;
+    result += '\n';
+  });
+
+  return result;//.replace(/\n +\n/g, '\n\n');
+};
+
+test('ascii draw', () => {
+  const data = [
+    [ 1, 1, 1, 0, 0 ],
+    [ 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 0, 0 ],
+    [ 1, 0, 1, 1, 0 ],
+    [ 1, 0, 1, 0, 0 ]
+  ]
+  const rowHints = [ [3], [5], [3], [1, 2], [1, 1] ]
+  const columnHints = [ [5], [3], [5], [1, 1], [1] ]
+  // console.log(draw(rowHints, columnHints, data))
+
+  const output = [
+    "       1 ",
+    "         ",
+    "    53511",
+    // "",
+    "  3 ███░░",
+    "  5 █████",
+    "  3 ███░░",
+    "1 2 █░██░",
+    "1 1 █░█░░",
+    ""
+  ].join('\n')
+
+  expect(draw(rowHints, columnHints, data)).toBe(output)
+})
+
+test('solvable 5x5 with one hint dimension', () => {
+  const rowHints = [[3], [3], [4], [3], [1]]
+  const columnHints = [[2], [2], [4], [3], [3]]
+  expect(solvable(rowHints, columnHints)).toBe(true)
+});
+
+test('solvable 5x5 and two hint dimension', () => {
+  const rowHints = [[3], [4], [1, 3], [1, 1], [2]]
+  const columnHints = [[2], [1, 1], [3, 1], [4], [3]]
+  expect(solvable(rowHints, columnHints)).toBe(true)
+});
+
+test('solvable 5x10 and two hint dimension', () => {
+  const rowHints = [[3], [2], [1, 2], [2, 2], [4, 1]]
+  const columnHints = [[2], [1], [0], [1, 1], [1, 1], [1, 1], [1, 1], [2], [2], [2]]
+  expect(solvable(rowHints, columnHints)).toBe(true)
+});
+
+test('solvable 15x10 and three hint dimension', () => {
+  const rowHints = [[1], [1, 1], [1, 1], [2, 3], [1, 3], [2, 2], [4, 4], [3, 2], [5, 4], [2, 2, 2], [1, 1, 3], [2, 4], [4, 3], [6, 1], [7, 1]]
+  const columnHints = [[3, 8], [2, 4, 4], [3, 1, 3], [2, 2, 4], [4, 4, 2], [5, 5], [4, 5, 1], [2, 2, 2], [3, 1], [3]]
+  expect(solvable(rowHints, columnHints)).toBe(true)
+});
+
+test('not solvable, multiple solutions', () => {
+  const rowHints = [[1], [4], [1, 1], [4], [1]]
+  const columnHints = [[1], [4], [1, 1], [4], [1]]
+  expect(solvable(rowHints, columnHints)).toBe(false)
+});
+
+test('not solvable with multiple solutions', () => {
+  const rowHints = [[1, 1], [1, 1, 1], [1, 1], [1], [1]]
+  const columnHints = [[2], [1, 1], [1, 1], [1, 1], [2]]
+  expect(solvable(rowHints, columnHints)).toBe(false)
+})
+
+test('not solvable, impossible', () => {
+  const rowHints = [[1, 1], [1, 1, 1], [1, 1], [1], [1]]
+  const columnHints = [[2], [3], [1, 1], [3], [2]]
+  expect(solvable(rowHints, columnHints)).toBe(false)
+})
+
+test('solvable from hints from a randomly generated puzzle', () => {
+  // was randomly generated and is solvable
+  const rowHints = [[5], [4], [4], [3, 1], [3], [1, 3], [2, 1], [2, 1], [2, 1], [5]]
+  const columnHints = [[6, 1], [5, 4], [10], [3, 1, 1], [1, 1, 5]]
+  expect(solvable(rowHints, columnHints)).toBe(true)
+});
+
+
+function generateRandomPuzzle(rng, rows, cols, values = 1, density = null) {
+  if (density === null) {
+    density = rng() * 0.6 + 0.2;
+  }
+
+  return Array.from({length: rows}, () => {
+    return Array.from({length: cols}, () => {
+      // const num = rng()
+      // if (num < density) {
+      //   return Math.floor(num * values) + 1
+      // }
+      if (rng() < density) {
+        return Math.floor(rng() * values) + 1
+      }
+      return 0
+    })
+  })
+
+  // let data = new Array(rows).fill();
+  // return data.map(() => {
+  //   return new Array(cols).fill(0).map(() => (rng() < density) ? Math.floor(rng() * values) + 1 : 0);
+  // })
+
+  // for (let row = 0; row < rows; row++) {
+  //   board[row] = new Array(cols).fill(0).map(_ => (rng() < density) ? Math.floor(rng() * values) + 1 : 0);
+  //   // for (let col = 0; col < cols; col++) {
+  //   //   if (rng() < density) {
+  //   //     board[row][col] = Math.floor(rng() * values) + 1;
+  //   //   }
+  //   // }
+  // }
+  // return board;
+}
+
+import getColumnHints from './utils/puzzle/getColumnHints';
+import getRowHints from './utils/puzzle/getRowHints';
+
+// test('seed random', () => {
+//   const expectedBoard = [
+//     [ 1, 1, 1, 1, 1 ],
+//     [ 0, 0, 0, 1, 0 ],
+//     [ 1, 0, 1, 1, 1 ],
+//     [ 1, 1, 1, 1, 0 ],
+//     [ 1, 0, 1, 1, 0 ]
+//   ]
+//   let rng = seedrandom('hello.');
+//   let data = generateRandomPuzzle(rng, 5, 5)
+//   expect(data).toStrictEqual(expectedBoard)
+//   expect(solvable(getRowHints(data), getColumnHints(data))).toBe(true)
+
+//   let board = generateRandomPuzzle(rng, 5, 15)
+//   // console.log(getColumnHints(board))
+//   // console.log(getRowHints(board))
+//   // TODO: revert back to using a class, since we will need
+//   // the puzzle solution to compare to when user attempts to solve
+//   expect(solvable(getRowHints(board), getColumnHints(board))).toBe(true)
+// })
+
+// test('seed random 2', () => {
+//   let rng = seedrandom('seed');
+//   let data = generateRandomPuzzle(rng, 15, 10)
+//   expect(solvable(getRowHints(data), getColumnHints(data))).toBe(true)
+
+//   let board = generateRandomPuzzle(rng, 10, 15)
+//   // console.log(getColumnHints(board))
+//   // console.log(getRowHints(board))
+//   // TODO: revert back to using a class, since we will need
+//   // the puzzle solution to compare to when user attempts to solve
+//   expect(solvable(getRowHints(board), getColumnHints(board))).toBe(true)
+// })
+
+test('seed many random puzzles', () => {
+  // let rng = seedrandom('Javier Julio');
+  let sizes = [ [5, 5], [5, 10], [10, 5], [10, 10], [5, 15], [15, 5] ]
+  let finalSizes = []
+  let result = Array.from({length: 30}, (_, i) => {
+    let rng = seedrandom(`${i + 1}`);
+    let [rows, columns] = sizes[Math.floor(rng() * sizes.length)]
+    finalSizes.push([ rows, columns ])
+
+    let data = generateRandomPuzzle(rng, rows, columns, 1, 0.7)
+    let isSolvable = solvable(getRowHints(data), getColumnHints(data))
+    if (rows === 10 && columns === 5) {
+      if (isSolvable) {
+        console.log(draw(getRowHints(data), getColumnHints(data), data))
+      }
+    }
+    return isSolvable
+  })
+  console.log('solvable', result.filter((value) => value).length)
+  console.log('not solvable', result.filter((value) => !value).length)
+  console.log(finalSizes.map(v => `${v[0]}x${v[1]}`))
+})
