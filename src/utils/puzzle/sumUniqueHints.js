8eeed99b352fe00ef1001d594ffe8773dc3aff9e
Javier Julio <jjfutbol@gmail.com>
2020-09-04 16:31:15 -0400

    Add method to sum up grouped hints in a given array

---
+++ src/utils/puzzle/sumUniqueHints.js
@@ -0,0 +1,17 @@
+const sumUniqueHints = (array, callback=null) => {
+  if (!callback) {
+    callback = (value => value === 1);
+  }
+
+  return array.reduce((memo, value, index, target) => {
+    if (callback(value)) {
+      memo[memo.length - 1] += 1
+    }
+    else if (callback(target[index + 1]) && memo[memo.length - 1] > 0) {
+      memo.push(0)
+    }
+    return memo
+  }, [0])
+};
+
+export default sumUniqueHints;
---
+++ src/utils/puzzle/sumUniqueHints.test.js
@@ -0,0 +1,31 @@
+import sumUniqueHints from './sumUniqueHints';
+
+test('with all zeros, returns zero as hint', () => {
+  const row = [ 0, 0, 0, 0, 0 ]
+  expect(sumUniqueHints(row)).toStrictEqual([0])
+});
+
+test('with leading and trailing zeros', () => {
+  const row = [ 0, 1, 0, 1, 0 ]
+  expect(sumUniqueHints(row)).toStrictEqual([1, 1])
+});
+
+test('with more leading and trailing zeros', () => {
+  const row = [ 0, 0, 0, 1, 0, 1, 1, 0, 0, 0 ]
+  expect(sumUniqueHints(row)).toStrictEqual([1, 2])
+});
+
+test('with leading and trailing filled values', () => {
+  const row = [ 1, 0, 1, 0, 1 ]
+  expect(sumUniqueHints(row)).toStrictEqual([1, 1, 1])
+});
+
+test('with all filled values, returns total as hint', () => {
+  const row = [1, 1, 1, 1, 1]
+  expect(sumUniqueHints(row)).toStrictEqual([5])
+})
+
+test('with 3 different filled values, returns 3, 2, 1 as hints', () => {
+  const row = [1, 1, 1, 0, 0, 1, 1, 0, 0, 1]
+  expect(sumUniqueHints(row)).toStrictEqual([3, 2, 1])
+});
