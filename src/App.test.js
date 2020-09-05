7d68cca0b5da3752b7e032f827f24922aea52d4d
Javier Julio <jjfutbol@gmail.com>
2020-09-05 18:46:31 -0400

    Add disabled tests due to jsdom/pointerevents
    These tests should work but issue is upstream with jsdom. While we can simulate pointer events, we can't specify any properties other than the event type. Everything else always comes through undefined.
    
    https://github.com/jsdom/jsdom/pull/2666#issuecomment-615977553
    https://github.com/testing-library/dom-testing-library/issues/558

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, createEvent } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event'
 import App from './App';
 
 test('renders column hint value of 5', () => {
@@ -12,4 +13,26 @@ test('title on first cell', () => {
   const { getByTitle } = render(<App />);
   const firstCell = getByTitle('row 1, column 1')
   expect(firstCell).toBeInTheDocument();
+
+  // userEvent.click(firstCell, { buttons: 1, pointerType: 'mouse' })
+
+  // this should work but does not, upstream issue with JSDom
+  // these properties do not get set on the event object but they should
+  // https://github.com/jsdom/jsdom/pull/2666#issuecomment-615977553
+  // https://github.com/testing-library/dom-testing-library/issues/558
+
+  // fireEvent.pointerDown(firstCell, { buttons: 1, pointerType: 'mouse' })
+  // expect(getByTitle('row 1, column 1')).toHaveClass('filled')
+
+  // const event = createEvent('pointerdown', firstCell, { buttons: 1, pointerType: 'mouse' })
+  // console.log(event.buttons)
 });
+
+// test('fires event with custom properties', () => {
+//   // Once jsdom is updated, this should output "Escape" to console
+//   const spy = jest.fn((event) => { console.log(event.type, event.key) })
+//   document.addEventListener('pointerdown', spy)
+//   fireEvent.pointerDown(document, { key: 'Escape' })
+//   expect(spy).toHaveBeenCalledTimes(1)
+//   document.removeEventListener('pointerdown', spy)
+// })
