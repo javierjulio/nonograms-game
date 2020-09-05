940a835a5d6083b1a4b0b9566918dd183c3a6c9a
Javier Julio <jjfutbol@gmail.com>
2020-09-05 14:58:07 -0400

    Simplify selection code for pointer events
    We can now remove a ton of code as our pointer event setup is much simpler. Also simplifies the logic for determining the cell state (e.g. filled, crossed).
    
    This is the final result of replacing the mouse/touch events with pointer events and maintaining the same behavior as before. The one difference is for all pointeres, when tapping the same cell multiple times, it toggles between the 3 states: filled, crossed and empty. This way it plays well on mobile using one hand. No other buttons necessary to toggle cell state selection for example.

--- src/App.js
+++ src/App.js
@@ -49,97 +49,19 @@ function App() {
     event.preventDefault()
   }
 
-  const LEFT_MOUSE_CLICK = 0
+  // for left mouse click its different on pointermove but right mouse click is the same
+  // https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events#Determining_button_states
+  const LEFT_MOUSE_CLICK = 1
   const RIGHT_MOUSE_CLICK = 2
 
-  // let clearSelection;
   let lastCell;
-
-  // const mouseMoveHandler = (event) => {
-  //   console.log('mousemove', event.button)
-
-  //   let cell = event.target.closest('.nonogram-cell')
-
-  //   if (!cell || lastCell === cell)
-  //     return;
-
-  //   if (event.button === LEFT_MOUSE_CLICK) {
-  //     if (clearSelection) {
-  //       cell.classList.remove('crossed', 'filled')
-  //     } else if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-  //       // cell.classList.remove('crossed')
-  //       cell.classList.add('filled')
-  //     }
-  //   }
-  //   else if (event.button === RIGHT_MOUSE_CLICK) {
-  //     if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-  //       // cell.classList.remove('filled')
-  //       cell.classList.add('crossed')
-  //     }
-  //   }
-
-  //   lastCell = cell
-  // }
-
-  // const mouseUpHandler = (event) => {
-  //   console.log('mouse up')
-  //   clearSelection = false;
-  //   document.removeEventListener('mousemove', mouseMoveHandler)
-  //   document.removeEventListener('mouseup', mouseUpHandler)
-
-  //   // reset answer each time
-  //   // set cell value to 0, otherwise all cells have to be updated
-  //   let answer = new Array(data.length).fill().map(() => new Array(data[0].length).fill(0))
-
-  //   document.querySelectorAll('.nonogram-cell').forEach((value, index) => {
-  //     let numCols = answer[0].length
-  //     let rowIndex = Math.floor(index / numCols)
-  //     let colIndex = index % numCols
-
-  //     if (value.classList.contains('filled')) {
-  //       answer[rowIndex][colIndex] = 1
-  //     }
-  //     else if (value.classList.contains('crossed')) {
-  //       answer[rowIndex][colIndex] = 0
-  //     }
-  //   })
-
-  //   console.log('answer', JSON.stringify(data) === JSON.stringify(answer))
-  // }
-
-  // const mouseDownHandler = (event) => {
-  //   console.log('mousedown', event.button)
-
-  //   let cell = event.target.closest('.nonogram-cell')
-  //   if (cell) {
-  //     if (event.button === LEFT_MOUSE_CLICK) {
-  //       if (cell.classList.contains('crossed') || cell.classList.contains('filled')) {
-  //         clearSelection = true;
-  //         cell.classList.remove('crossed', 'filled')
-  //       }
-  //       else {
-  //         cell.classList.add('filled')
-  //         cell.classList.remove('crossed')
-  //       }
-  //     }
-  //     else if (event.button === RIGHT_MOUSE_CLICK) {
-  //       cell.classList.add('crossed')
-  //       cell.classList.remove('filled')
-  //     }
-
-  //     if (event.button === LEFT_MOUSE_CLICK || event.button === RIGHT_MOUSE_CLICK) {
-  //       document.addEventListener('mousemove', mouseMoveHandler)
-  //       document.addEventListener('mouseup', mouseUpHandler)
-  //     }
-  //   }
-  // }
 
   const EMPTY = -1;
   const CROSSED = 0;
   const FILLED = 1;
   let cellState;
 
-  const updateCellForState = (cell, state, override=true) => {
+  const updateCellForState = (cell, state) => {
     if (state === FILLED) {
       cell.classList.remove('crossed')
       cell.classList.add('filled')
@@ -153,168 +75,56 @@ function App() {
     }
   }
 
+  const getCellFromEvent = (event) => {
+    if (event.pointerType === 'mouse')
+      return event.target.closest('.nonogram-cell')
+
+    let target = document.elementFromPoint(event.clientX, event.clientY)
+    return target.closest('.nonogram-cell')
+  }
+
   const pointerDownHandler = (event) => {
     console.log('pointerDown', event.pointerType, event.button, event.buttons)
     lastCell = null
     cellState = null
 
-    let cell;
-    if (event.pointerType === 'mouse') {
-      cell = event.target.closest('.nonogram-cell')
-    } else {
-      let target = document.elementFromPoint(event.clientX, event.clientY);
-      cell = target.closest('.nonogram-cell')
-    }
+    let cell = getCellFromEvent(event)
 
     if (!cell)
       return;
 
-    if (event.pointerType === 'mouse') {
-      if (event.button === LEFT_MOUSE_CLICK) {
-        if (cell.classList.contains('crossed')) {
-          // clearSelection = true;
-          // cell.classList.remove('crossed')
-          cellState = EMPTY
-        }
-        else if (cell.classList.contains('filled')) {
-          // cell.classList.replace('filled', 'crossed')
-          cellState = CROSSED
-        }
-        else {
-          // cell.classList.add('filled')
-          cellState = FILLED
-        }
-
-        // if (cell.classList.contains('crossed') || cell.classList.contains('filled')) {
-        //   clearSelection = true;
-        //   cell.classList.remove('crossed', 'filled')
-        // }
-        // else {
-        //   cell.classList.add('filled')
-        //   cell.classList.remove('crossed')
-        // }
-      }
-      else if (event.button === RIGHT_MOUSE_CLICK) {
-        // cell.classList.add('crossed')
-        // cell.classList.remove('filled')
-        cellState = CROSSED
-      }
-    }
-
-    if (event.pointerType === 'touch') {
+    if (event.buttons === LEFT_MOUSE_CLICK) { // same value for left mouse, touch contact, pen contact
       if (cell.classList.contains('crossed')) {
-        // clearSelection = true;
-        // cell.classList.remove('crossed')
         cellState = EMPTY
-
       }
       else if (cell.classList.contains('filled')) {
-        // cell.classList.replace('filled', 'crossed')
         cellState = CROSSED
       }
       else {
-        // cell.classList.add('filled')
         cellState = FILLED
       }
-
-      // if (event.button === LEFT_MOUSE_CLICK) {
-        // if (cell.classList.contains('crossed') || cell.classList.contains('filled')) {
-        //   clearSelection = true;
-        //   cell.classList.remove('crossed', 'filled')
-        // }
-        // else {
-        //   cell.classList.add('filled')
-        //   cell.classList.remove('crossed')
-        // }
-      // }
-      // else if (event.button === RIGHT_MOUSE_CLICK) {
-      //   cell.classList.add('crossed')
-      //   cell.classList.remove('filled')
-      // }
+    }
+    else if (event.buttons === RIGHT_MOUSE_CLICK) {
+      cellState = CROSSED
     }
 
     if (cellState !== null) {
       updateCellForState(cell, cellState)
+      document.addEventListener('pointermove', pointerMoveHandler)
+      document.addEventListener('pointerup', pointerUpHandler)
     }
-
-    document.addEventListener('pointermove', pointerMoveHandler)
-    document.addEventListener('pointerup', pointerUpHandler)
   }
 
   const pointerMoveHandler = (event) => {
-    console.log('pointerMove', event.pointerType, event.buttons)
+    console.log('pointerMove', event.pointerType, event.button, event.buttons)
 
-    let cell;
-    if (event.pointerType === 'mouse') {
-      cell = event.target.closest('.nonogram-cell')
-    } else {
-      let target = document.elementFromPoint(event.clientX, event.clientY);
-      cell = target.closest('.nonogram-cell')
-    }
+    let cell = getCellFromEvent(event)
 
-    if (!cell || lastCell === cell) {
+    if (!cell || lastCell === cell)
       return;
-    }
 
-    if (event.pointerType === 'mouse') {
-      if (event.buttons === 1) { // for left mouse click (different on pointermove) https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events#Determining_button_states
-        if(cellState !== EMPTY && (cell.classList.contains('crossed') || cell.classList.contains('filled'))) {
-        }
-        else {
-          updateCellForState(cell, cellState)
-        }
-        // if (clearSelection) {
-        //   cell.classList.remove('crossed', 'filled')
-        // }
-        // if (cell.classList.contains('crossed')) {
-        //   clearSelection = true;
-        //   cell.classList.remove('crossed')
-        // }
-        // else if (cell.classList.contains('filled')) {
-        //   cell.classList.replace('filled', 'crossed')
-        //   cell.classList.add('crossed')
-        // }
-        // else {
-        //   cell.classList.add('filled')
-        // }
-
-        // if (clearSelection) {
-        //   cell.classList.remove('crossed', 'filled')
-        // } else if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-        //   // cell.classList.remove('crossed')
-        //   cell.classList.add('filled')
-        // }
-      }
-      else if (event.buttons === RIGHT_MOUSE_CLICK) { // stays the same with pointermove
-        if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-          // cell.classList.remove('filled')
-          // cell.classList.add('crossed')
-          updateCellForState(cell, cellState)
-        }
-      }
-    }
-
-    if (event.pointerType === 'touch') {
-      if(cellState !== EMPTY && (cell.classList.contains('crossed') || cell.classList.contains('filled'))) {
-      }
-      else {
-        updateCellForState(cell, cellState)
-      }
-
-      // if (event.buttons === 1) { // for left mouse click (different on pointermove) https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events#Determining_button_states
-        // if (clearSelection) {
-        //   cell.classList.remove('crossed', 'filled')
-        // } else if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-        //   // cell.classList.remove('crossed')
-        //   cell.classList.add('filled')
-        // }
-      // }
-      // else if (event.buttons === RIGHT_MOUSE_CLICK) { // stays the same with pointermove
-      //   if (!cell.classList.contains('crossed') && !cell.classList.contains('filled')) {
-      //     // cell.classList.remove('filled')
-      //     cell.classList.add('crossed')
-      //   }
-      // }
+    if (cellState === EMPTY || (!cell.classList.contains('crossed') && !cell.classList.contains('filled'))) {
+      updateCellForState(cell, cellState)
     }
 
     lastCell = cell
@@ -328,10 +138,12 @@ function App() {
 
   const pointerUpHandler = (event) => {
     console.log('pointerUp');
-    lastCell = null;
-    cellState = null;
+
     document.removeEventListener('pointermove', pointerMoveHandler)
     document.removeEventListener('pointerup', pointerUpHandler)
+
+    cellState = null;
+    lastCell = null;
 
     // 1) Reset answer each time
     // 2) Set cell default value to 0, otherwise all cells have to
@@ -367,7 +179,6 @@ function App() {
           <HintGroups data={rowHints} />
         </div>
         <div className="nonogram-grid" onTouchEnd={disableTouchAction} onPointerDown={pointerDownHandler} onContextMenu={contextMenuHandler}>
-        {/* <div className="nonogram-grid" onPointerDown={pointerDownHandler} onMouseDown={mouseDownHandler} onContextMenu={contextMenuHandler} onTouchStart={touchStartHandler}> */}
           {renderPuzzle(data)}
         </div>
       </div>
