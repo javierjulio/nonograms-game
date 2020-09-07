cc258bf3e0c5cd4038dea82d5db49dbf7a01ccf7
Javier Julio <jjfutbol@gmail.com>
2020-09-07 15:41:26 -0400

    Update column hint test case
    Since we changed the default puzzle in our app, there are multiple hints of 5 so this throws an error if multiple matches are found. This isn't a helpful test much if we change the puzzle but just to have something here for now so we can expand and learn how to with Jest/client side.

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import { render, fireEvent, createEvent } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders column hint value of 5', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText("5");
-  expect(linkElement).toBeInTheDocument();
+test('renders a hint value of 5', () => {
+  const { getAllByText } = render(<App />);
+  const hints = getAllByText("5");
+  hints.map(el => expect(el).toBeInTheDocument());
 });
 
 test('title on first cell', () => {
