8163e655b2228948880e932bed5acec1036ff3e3
Javier Julio <jjfutbol@gmail.com>
2021-04-18 01:31:09 -0400

    Update grid cell to use SVG for X (crossed) display

--- src/components/GridCell.js
+++ src/components/GridCell.js
@@ -1,13 +1,20 @@
 import { memo } from "react";
+import { ReactComponent as XIcon } from "../svgs/x.svg";
 
 const GridCell = memo(({value, row, column}) => {
   const classStates = [ "crossed", "filled" ]
-  const className = classStates[value] || ""
+  const className = classStates[value] || "unknown"
+
+  let contents = ""
+  if (className === "crossed") {
+    contents = <XIcon />
+  }
 
   return (
     <div className={`nonogram-cell ${className}`}
       title={`row ${row+1}, column ${column+1}`}
       data-row={row} data-column={column}>
+      { contents }
     </div>
   )
 })
